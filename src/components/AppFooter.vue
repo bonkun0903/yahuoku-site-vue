0d52535a83b96b1799cdba744c45de2c72f20f41
hiroagritech <plutonium.090301@gmail.com>
2021-10-04 09:52:54 +0900

    Add new components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer
+    dark
+    absolute
+  >
+    <div class="footer">
+      Copyright test 2020. All right reserved
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  margin: auto;
+}
+</style>
