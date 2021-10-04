8894705bbba41f928559e9bca83decc1a9bced42
hiroagritech <plutonium.090301@gmail.com>
2021-10-04 12:00:03 +0900

    Add work-page component

--- src/common/constants.js
+++ src/common/constants.js
@@ -24,5 +24,19 @@ export default {
       name: 'ACCESS',
       url: '/access'
     }
+  ],
+  workItems: [
+    {
+      src: require('../assets/img/work-img-1.jpg'),
+      url: '/work#work-1'
+    },
+    {
+      src: require('../assets/img/work-img-2.jpg'),
+      url: '/work#work-2'
+    },
+    {
+      src: require('../assets/img/work-img-3.jpg'),
+      url: '/work#work-3'
+    }
   ]
 }
